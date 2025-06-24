3f74daf348ca33da5cb4e46fbc121d283a2653a2
mosheilan942 <moisi6510@gmail.com>
2025-06-24 18:11:26 +0300

    init commit

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorldApp
+{
+    public class Message
+    {
+        public string GetMessage() => "Hello World!";
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var message = new Message();
+            Console.WriteLine(message.GetMessage());
+        }
+            
+    }
+}
